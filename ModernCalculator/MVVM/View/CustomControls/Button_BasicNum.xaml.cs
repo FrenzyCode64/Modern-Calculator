e6151792d3de21fdc7aac91814ce2f5ed0a9501b
FrenzyCode64 <divanoff228@gmail.com>
2023-06-10 12:26:43 +0300

    commit-button-design-added

---
+++ ModernCalculator/MVVM/View/CustomControls/Button_BasicNum.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernCalculator.MVVM.View.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for Button_BasicNum.xaml
+    /// </summary>
+    public partial class Button_BasicNum : UserControl
+    {
+        public static readonly DependencyProperty ValueNumberProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(Button_BasicNum), 
+                new PropertyMetadata(0));
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueNumberProperty); }
+            set { SetValue(ValueNumberProperty, value); }
+        }
+
+        public Button_BasicNum()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ModernCalculator/MVVM/View/CustomControls/Button_Termination.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernCalculator.MVVM.View.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for Button_Termination.xaml
+    /// </summary>
+    public partial class Button_Termination : UserControl
+    {
+        public static readonly DependencyProperty OperationNumberProperty =
+            DependencyProperty.Register("Operation", typeof(string), typeof(Button_Termination),
+                new PropertyMetadata(string.Empty));
+
+        public string Operation
+        {
+            get { return (string)GetValue(OperationNumberProperty); }
+            set { SetValue(OperationNumberProperty, value); }
+        }
+
+        public Button_Termination()
+        {
+            InitializeComponent();
+        }
+    }
+}
