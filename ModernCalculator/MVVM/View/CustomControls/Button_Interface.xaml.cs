d7f0f08bed1ff76dbd9b04fb8e8e696edc824fda
FrenzyCode64 <divanoff228@gmail.com>
2023-06-14 16:34:34 +0300

    commit-hotfix-aboutmenu

---
+++ ModernCalculator/MVVM/View/CustomControls/Button_Interface.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ModernCalculator.MVVM.View.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for Button_Interface.xaml
+    /// </summary>
+    public partial class Button_Interface : UserControl
+    {
+        public static readonly DependencyProperty ExitStateProperty =
+            DependencyProperty.RegisterAttached("ExitState", typeof(int), typeof(Button_Interface), new PropertyMetadata(0));
+
+        public int ExitState
+        {
+            get { return (int)GetValue(ExitStateProperty); }
+            set { SetValue(ExitStateProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextElProperty =
+            DependencyProperty.RegisterAttached("TextField", typeof(string), typeof(Button_Interface), new PropertyMetadata(string.Empty));
+
+        public string TextField
+        {
+            get { return (string)GetValue(TextElProperty); }
+            set { SetValue(TextElProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundElProperty =
+            DependencyProperty.RegisterAttached("BackGroundColor", typeof(Brush), typeof(Button_Interface), new PropertyMetadata(Brushes.Transparent));
+
+        public Brush BackGroundColor
+        {
+            get { return (Brush)GetValue(BackgroundElProperty); }
+            set { SetValue(BackgroundElProperty, value); }
+        }
+
+        public static readonly DependencyProperty ForegroundElProperty =
+            DependencyProperty.RegisterAttached("ForeGroundColor", typeof(Brush), typeof(Button_Interface), new PropertyMetadata(Brushes.Transparent));
+
+        public Brush ForeGroundColor
+        {
+            get { return (Brush)GetValue(ForegroundElProperty); }
+            set { SetValue(ForegroundElProperty, value); }
+        }
+
+        public static readonly RoutedEvent Click_RoutedEvent = EventManager.RegisterRoutedEvent(nameof(EventClick), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Button_Interface));
+        public event RoutedEventHandler EventClick
+        {
+            add { AddHandler(Click_RoutedEvent, value); }
+            remove { RemoveHandler(Click_RoutedEvent, value); }
+        }
+
+        public Button_Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(Click_RoutedEvent));
+        }
+    }
+}
--- ModernCalculator/MainWindow.xaml.cs
+++ ModernCalculator/MainWindow.xaml.cs
@@ -2,9 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 
 namespace ModernCalculator
 {
@@ -19,9 +24,20 @@ namespace ModernCalculator
         public int PropertyValue_enter
         {
             get { return value_enter; }
-            set 
-            { 
+            set
+            {
                 value_enter = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string string_term;
+        public string PropertyString_term
+        {
+            get { return string_term; }
+            set
+            {
+                string_term = value;
                 NotifyPropertyChanged();
             }
         }
@@ -30,14 +46,13 @@ namespace ModernCalculator
         public int PropertyCurrent_operand
         {
             get { return current_operand; }
-            set 
-            { 
+            set
+            {
                 current_operand = value;
                 if (PropertyCurrent_operand >= OPERAND_LIST_SIZE)
-                    PropertyCurrent_operand = PropertyCurrent_operand - OPERAND_LIST_SIZE;
+                    PropertyCurrent_operand -= OPERAND_LIST_SIZE;
             }
         }
-
 
         private const int OPERAND_LIST_SIZE = 2;
         private int[] operand_info = new int[OPERAND_LIST_SIZE];
@@ -50,12 +65,16 @@ namespace ModernCalculator
 
             PropertyValue_enter = Convert.ToInt32(PropertyValue_enter.ToString() + button_sender.Value.ToString());
             operand_info[PropertyCurrent_operand] = PropertyValue_enter;
+
+            Update_TermString();
         }
 
         private void Click_Operation(object sender, RoutedEventArgs event_args)
         {
             Button_OperationSign button_sender = (Button_OperationSign)sender;
+
             operation_sign = button_sender.Operation[0];
+            PropertyString_term += button_sender.Operation[0];
 
             PropertyCurrent_operand++;
             PropertyValue_enter = 0;
@@ -80,6 +99,8 @@ namespace ModernCalculator
 
             operand_info[0] = PropertyValue_enter;
             PropertyCurrent_operand++;
+
+            Update_TermString();
         }
 
         public void Click_TerminateLastNum(object sender, RoutedEventArgs event_args)
@@ -92,10 +113,43 @@ namespace ModernCalculator
 
         private void Click_TerminateAll(object sender, RoutedEventArgs event_args)
         {
-            PropertyCurrent_operand = 0;
             PropertyValue_enter = 0;
+            for (int a = 0; a < OPERAND_LIST_SIZE; a++)
+                operand_info[a] = 0;
 
             operation_sign = '\0';
+        }
+
+        private void Click_AboutButton(object sender, RoutedEventArgs event_args)
+        {
+            const double time_animation = 0.5; 
+
+            Button_Interface button_sender = (Button_Interface)sender;
+            Panel.SetZIndex(border_AboutMenu, button_sender.ExitState);
+
+            if (button_sender.ExitState == 1)
+                Animate_panel(true, false);
+            else if (button_sender.ExitState == -1)
+                Animate_panel(false, true);
+
+            void Animate_panel(bool ToPanel, bool ToMain)
+            {
+                border_AboutMenu.BeginAnimation(UIElement.OpacityProperty, new DoubleAnimation(Convert.ToDouble(ToPanel), TimeSpan.FromSeconds(time_animation)));
+                border_AboutMenu.IsEnabled = ToPanel;
+
+                grid_UserMenu.BeginAnimation(UIElement.OpacityProperty, new DoubleAnimation(Convert.ToDouble(ToMain), TimeSpan.FromSeconds(time_animation)));
+                grid_UserMenu.IsEnabled = ToMain;
+            }
+        }
+
+        private void Update_TermString()
+        {
+            for (int a = 0; a < OPERAND_LIST_SIZE; a++)
+            {
+                PropertyString_term += operand_info[a].ToString();
+                if (a < OPERAND_LIST_SIZE - 1)
+                    PropertyString_term += operation_sign;
+            }
         }
 
         private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -104,10 +158,21 @@ namespace ModernCalculator
                 DragMove();
         }
 
+        private void Site_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Image image_sender = (Image)sender;
+            switch (image_sender.Name)
+            {
+                case "icon_GitHub": Process.Start("https://github.com/FrenzyCode64"); break;
+                case "icon_Steam": Process.Start("https://steamcommunity.com/profiles/76561198931354092"); break;
+                case "icon_Youtube": Process.Start("https://www.youtube.com/channel/UC1eDs-42u2UZ9A0ykDFXl4g"); break;
+            }
+        }
+
         public MainWindow()
         {
-            DataContext = this;
             InitializeComponent();
+            DataContext = this;
         }
 
         protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
